293960fc803acf5afaa1e77a0f150f1115a5dd5d
Andres Tellez <andres.tezper@gmail.com>
2025-08-21 03:19:14 -0600

    feat: removing eslint rule @nx/enforce-module-boundaries

--- apps/auth-service/eslint.config.mjs
+++ apps/auth-service/eslint.config.mjs
@@ -1,26 +1,3 @@
 import baseConfig from '../../eslint.config.mjs';
 
-export default [
-	...baseConfig,
-	{
-		rules: {
-			'@nx/enforce-module-boundaries': [
-				'error',
-				{
-					enforceBuildableLibDependency: true,
-					allow: [
-						'^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$',
-						// TODO: Whitelist Folders
-						'../../packages'
-					],
-					depConstraints: [
-						{
-							sourceTag: '*',
-							onlyDependOnLibsWithTags: ['*']
-						}
-					]
-				}
-			]
-		}
-	}
-];
+export default [...baseConfig];
